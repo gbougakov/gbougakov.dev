744c264c5e19434d203b6c623f8f15b8f43e0164
George Bougakov <g@bougakov.com>
2020-03-31 21:26:13 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
-const tailwindcss = require('tailwindcss')
-
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')]
+  plugins: [
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    'postcss-preset-env',
+  ],
 }
