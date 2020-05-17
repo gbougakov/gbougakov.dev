345f5d79f07ae0f9ba30f6e0545df351708283ea
George Bougakov <githubinq@bygeorgenet.me>
2020-05-17 12:55:52 +0200

    hm

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import '../styles/style.css'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
