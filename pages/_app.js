9548e7e5544c37efdb1a62e7a4a9871090517d2b
George Bougakov <githubinq@bygeorgenet.me>
2020-05-19 17:56:34 +0200

    Fix GA

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,13 @@ import '../styles/style.css'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import ReactGA from 'react-ga'
-ReactGA.initialize('UA-140464182-3', { debug: true })
+ReactGA.initialize('UA-140464182-3')
 
-Router.events.on('routeChangeStart', url => {
-  console.log(`Loading: ${url}`)
-  NProgress.start()
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', url => {
+  NProgress.done()
   ReactGA.pageview(url)
 })
-Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
