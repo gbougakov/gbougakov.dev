5db08775b6a23d45151af948fcdb479c469d48a4
George Bougakov <githubinq@bygeorgenet.me>
2020-05-19 17:58:47 +0200

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,22 @@ import '../styles/style.css'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import ReactGA from 'react-ga'
+import { useEffect } from 'react'
 ReactGA.initialize('UA-140464182-3')
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', url => {
   NProgress.done()
+  ReactGA.set({ page: url })
   ReactGA.pageview(url)
 })
 Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.set({ page: window.location.pathname })
+    ReactGA.pageview(window.location.pathname)
+  }, [])
   return <Component {...pageProps} />
 }
 
