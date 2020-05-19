0c2be6ecb1a45bf62f6af8e02cb222633efcec6c
George Bougakov <githubinq@bygeorgenet.me>
2020-05-19 17:40:47 +0200

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/style.css'
 import Router from 'next/router'
 import NProgress from 'nprogress'
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-140464182-3', { debug: true })
 
 Router.events.on('routeChangeStart', url => {
   console.log(`Loading: ${url}`)
   NProgress.start()
+  ReactGA.pageview(url)
 })
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
